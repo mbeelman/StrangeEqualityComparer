716f37bb99042b9f3caba417a4b79c3a1dc2c5ac
Marko Beelmann <marko.beelmann@philips.com>
2016-12-08 21:25:10 +0100

    Inital commit.

---
+++ Library/StructWithValue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Library
+{
+    public struct StructWithValue : IEquatable<StructWithValue>
+    {
+        private ushort value;
+        public StructWithValue(ushort v)
+        {
+            value = v;
+        }
+        public override int GetHashCode()
+        {
+            return value.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            return obj != null && !(base.GetType() != obj.GetType()) && this.Equals((StructWithValue)obj);
+        }
+
+        public bool Equals(StructWithValue other)
+        {
+            return value.Equals(other.value);
+        }
+
+        public static bool operator ==(StructWithValue a1, StructWithValue a2)
+        {
+            return a1.Equals(a2);
+        }
+
+        public static bool operator !=(StructWithValue a1, StructWithValue a2)
+        {
+            return !(a1 == a2);
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // get the default comparer for this type
+            var comparer = EqualityComparer<StructWithValue>.Default;
+
+            // loop through the full ushort range.
+            for (ushort i = ushort.MinValue; i <= ushort.MaxValue; i++)
+            {
+                var a = new StructWithValue(i);
+                var b = new StructWithValue(i);
+
+                if (comparer.Equals(a, b))
+                    continue;
+
+                // the value of 32768 will show this message and end the loop
+                // every value > 32767 is affected...
+                Console.WriteLine($"Problem start at {i}");
+                break;
+            }
+        }
+    }
+}
